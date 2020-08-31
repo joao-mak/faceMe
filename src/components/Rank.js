52451d1789295cbd8ba23f67fd3a312bc6812c3e
joao-mak <joao_mak@msn.com>
2020-08-31 18:42:39 +0100

    change rank to score

--- src/components/Rank.js
+++ src/components/Rank.js
@@ -4,7 +4,7 @@ const Rank = (props) => {
   const { name, entries } = props;
   return (
     <div className="rank">
-      <p className="rank-text-1">{`${name}, your current rank is...`}</p>
+      <p className="rank-text-1">{`${name}, your current score is...`}</p>
       <p className="rank-text-2">{`${entries}`}</p>
     </div>
   );
