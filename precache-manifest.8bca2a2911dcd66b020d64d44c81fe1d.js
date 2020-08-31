0c11ca04c63c98f99da9b71277cd87c555094c0c
joao-mak <joao_mak@msn.com>
2020-08-31 18:19:12 +0100

    Updates

---
+++ precache-manifest.8bca2a2911dcd66b020d64d44c81fe1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b3684a5e7c94d4d13c90cc73533dd5",
+    "url": "/faceMe/index.html"
+  },
+  {
+    "revision": "4a33479b8322767652c4",
+    "url": "/faceMe/static/css/main.538668f6.chunk.css"
+  },
+  {
+    "revision": "e083f2ba0a7ce665247f",
+    "url": "/faceMe/static/js/2.b8e6ea34.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/faceMe/static/js/2.b8e6ea34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a33479b8322767652c4",
+    "url": "/faceMe/static/js/main.08eea5aa.chunk.js"
+  },
+  {
+    "revision": "9569591566d4e04965ae",
+    "url": "/faceMe/static/js/runtime-main.c9aecd34.js"
+  }
+]);
